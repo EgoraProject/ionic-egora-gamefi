5f673bf29c141979b98bb774d71eddbabf9457af
Rasha Saadeh <>
2022-09-09 21:39:30 -0400

    sentry integration

--- src/index.tsx
+++ src/index.tsx
@@ -9,19 +9,21 @@ import { BrowserTracing } from '@sentry/tracing'
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
+Sentry.init({
+  // what sentry project the data should be sent to
+  dsn: 'https://74689645d71f46b4aa1d2c60b7f64ea3@o1379622.ingest.sentry.io/6692556', 
+  release: "sentry-demo@0.5.0",
+  dist: "1",
+  tracesSampleRate: 1,
+  integrations: [new BrowserTracing()]
+}, SentryReact.init)
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-Sentry.init({
-  // what sentry project the data should be sent to
-  dsn: process.env.SENTRY_DSN, 
-  release: "sentry-demo@0.4.0",
-  dist: "1",
-  tracesSampleRate: 1,
-  integrations: [new BrowserTracing]
-}, SentryReact.init)
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
